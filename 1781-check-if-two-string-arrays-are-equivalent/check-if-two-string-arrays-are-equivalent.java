51501cd048ff9c3332df678b050d2a55b02e3ad9
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-06-20 18:20:20 -0700

    Time: 3 ms (11.81%) | Memory: 41.2 MB (68.35%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,38 +1,29 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String s1 = "";
-        String s2 = "";
+        String s1="";
+        String s2="";
 
-        // Concatenate all strings in word1 into s1
-        for (int i = 0; i < word1.length; i++) {
-            s1 += word1[i];
+        for(int i=0;i<word1.length;i++){
+            s1+=word1[i];
         }
 
-        // Concatenate all strings in word2 into s2
-        for (int i = 0; i < word2.length; i++) {
-            s2 += word2[i];
+        for(int i=0;i<word2.length;i++){
+            s2+=word2[i];
         }
 
-        // Check if the lengths are different
-        if (s1.length() != s2.length()) {
-            return false;
-        }
+        int i=0;
+        int j=0;
 
-        // Compare the characters of both strings
-        int i = 0;
-        int j = 0;
-
-        while (i < s1.length() && j < s2.length()) {
-            if (s1.charAt(i) == s2.charAt(j)) {
+        while(i<s1.length() && j<s2.length ()){
+            if(s1.charAt(i)==s2.charAt(j)){
                 i++;
                 j++;
-            } else {
+            }else{
                 return false;
             }
         }
 
-        // Both strings should be fully traversed
-        return i == s1.length() && j == s2.length();
+         return (i==s1.length ()&& j==s2.length());
 
     }
 }
