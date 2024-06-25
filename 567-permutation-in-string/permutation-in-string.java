68f97369b789f7b4a2741bc5cd07b059a1951331
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-06-24 17:11:38 -0700

    Time: 27 ms (33.14%) | Memory: 42.8 MB (43.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,38 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        int count=map.size();
-        int k=s1.length();
-        int i=0;
-        int j=0;
-        while(j<s2.length()){
-            if(map.containsKey(s2.charAt(j))){
+       HashMap<Character,Integer> map=new HashMap<>();
+       for(int i=0;i<s1.length();i++){
+        map.put(s1.charAt(i),map.getOrDefault(s1.charAt(i),0)+1);
+       }
+       int k=s1.length();
+       int i=0;
+       int j=0;
+       int count=map.size();
+
+       while(j<s2.length()){
+        if(map.containsKey(s2.charAt(j))){
                 map.put(s2.charAt(j),map.get(s2.charAt(j))-1);
                 if(map.get(s2.charAt(j))==0){
                     count--;
                 }
-            }
-            if(j-i+1 < k){
-                j++;
-              }else if(j-i+1 == k){
-                if(count==0){
-                    return true;
-                }
+        }
 
-                if(map.containsKey(s2.charAt(i))){
+        if(j-i+1<k){
+            j++;
+        }else if(j-i+1==k){
+            if(count==0) return true;
+            if(map.containsKey(s2.charAt(i))){
                     map.put(s2.charAt(i),map.get(s2.charAt(i))+1);
                     if(map.get(s2.charAt(i))==1){
                         count++;
                     }
                 }
-                i++;
-                j++;
-            }
+            i++;
+            j++;
         }
-        return false;
+       }
+       return false;
+
     }
 }
