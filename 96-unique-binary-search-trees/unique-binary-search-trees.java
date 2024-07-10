c1ffaac239400512aaff714701e6eae47d7d5fb7
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-10 15:57:01 -0700

    Time: 0 ms (100.00%) | Memory: 40.4 MB (25.97%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -3,12 +3,13 @@ class Solution {
     public int numTrees(int n) {
         if(n<=1) return 1;
 
+        if(memo[n]!= 0) return memo[n];
         int ans=0;
 
         for(int i=1;i<=n;i++){
             ans+=numTrees(i-1)*numTrees(n-i);
         }
 
-        return ans;
+        return memo[n]=ans;
     }
 }
