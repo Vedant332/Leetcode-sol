ad1af8c50db5b93e6c06e1e54b0f9c01ed16ae80
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-17 16:11:05 -0700

    Time: 1 ms (78.00%) | Memory: 41.4 MB (68.64%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,20 +1,38 @@
 class Solution {
+    String ans="";
     public String findDifferentBinaryString(String[] nums) {
-         int n = nums.length;
-        // HashSet<String> hs = new HashSet<>();
-        // for (String num : nums) {
-        //     hs.add(num);
-        // }
-        
+         HashSet<String> hs = new HashSet<>();
+        for (String num : nums) {
+            hs.add(num);
+        }
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (nums[i].charAt(i) == '0') {
-                sb.append('1');
+        helper(nums.length, hs, 0, sb);
+        return ans;
+    }
+
+    public boolean helper(int n, HashSet<String> hs, int ind, StringBuilder sb) {
+        if (ind == n) {
+            String candidate = sb.toString();
+            if (!hs.contains(candidate)) {
+                ans = candidate;
+                return true;
             } else {
-                sb.append('0');
+                return false;
             }
         }
-        
-        return sb.toString();
+
+        sb.append('0');
+        if (helper(n, hs, ind + 1, sb)) {
+            return true;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        sb.append('1');
+        if (helper(n, hs, ind + 1, sb)) {
+            return true;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        return false;
     }
 }
