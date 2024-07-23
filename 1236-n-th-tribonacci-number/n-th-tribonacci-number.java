fb8601c85b81b58d5e7373a8fb6fb816ff232790
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-23 12:13:09 -0700

    Time: 0 ms (100.00%) | Memory: 40.3 MB (40.78%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,18 +1,28 @@
 class Solution {
     public int tribonacci(int n) {
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return func(n,dp);
-    }
-
-    public int func(int n,int[] dp){
-        if(n==0){
-            return 0;
-        }
+        if(n==0) return 0;
         if(n==1||n==2){
             return 1;
         }
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=func(n-1,dp)+func(n-2,dp)+func(n-3,dp);
+        int[] dp=new int[n+1];
+        // Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
     }
+
+    // public int func(int n,int[] dp){
+    //     if(n==0){
+    //         return 0;
+    //     }
+    //     if(n==1||n==2){
+    //         return 1;
+    //     }
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=func(n-1,dp)+func(n-2,dp)+func(n-3,dp);
+    // }
 }
