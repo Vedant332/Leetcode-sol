3e93d4a0234c493bfcf6b5373caeb6ac37ac8c06
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-29 14:33:40 -0700

    Time: 2 ms (34.01%) | Memory: 40.8 MB (59.74%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -3,7 +3,17 @@ class Solution {
         Arrays.sort(nums);
         int []dp= new int[target+1]; 
         Arrays.fill(dp,-1);
-        return helper(nums,target,dp);
+        dp[0]=1;
+        for(int tar=1;tar<target+1;tar++){
+            int ans=0;
+        for(int i=0;i<nums.length;i++){
+           if(nums[i]<=tar){
+              ans+=dp[tar-nums[i]];
+           }
+        }
+        dp[tar]=ans;
+        }
+        return dp[target];
     }
 
     public int helper(int[] nums,int target,int[] dp){
