1059bad9bce16e0beeb6f3e2a33ff5ba221044d4
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-29 15:18:36 -0700

    Time: 21 ms (89.59%) | Memory: 42.3 MB (85.60%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,8 +1,21 @@
 class Solution {
     public int numSquares(int n) {
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
+        Arrays.fill(dp,0);
+
+        dp[0]=0;
+
+        for(int j=1;j<n+1;j++){
+            int mini=Integer.MAX_VALUE;
+            int ans=0;
+        for(int i=1;i*i<=j;i++){
+                ans=dp[j-i*i]+1;
+            
+            mini=Math.min(mini,ans);
+        }
+         dp[j]=mini;
+        }
+        return dp[n];
     }
 
     public int helper(int n,int[] dp){
