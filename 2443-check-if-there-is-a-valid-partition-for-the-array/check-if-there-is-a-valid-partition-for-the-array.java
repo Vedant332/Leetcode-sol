c6c5382169336314990f07693b331564411244b5
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-30 13:07:24 -0700

    Time: 4 ms (100.00%) | Memory: 62.7 MB (39.76%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean validPartition(int[] nums) {
-        return func(nums ,nums.length);
+        boolean[] dp=new boolean[nums.length+1];
+        return func(nums ,nums.length,dp);
     }
 
-    public boolean func(int[] nums,int ind){
+    public boolean func(int[] nums,int ind,boolean[] dp){
         if(ind==0){
             return true;
         }
 
-        if(ind>=2 && nums[ind-2]==nums[ind-1] && func(nums,ind-2)){
-            return true;
+        if(dp[ind]!=false) return dp[ind];
+
+        if(ind>=2 && nums[ind-2]==nums[ind-1] && func(nums,ind-2,dp)){
+            return dp[ind]=true;
         }
         if(ind>=3 && (nums[ind-3]==nums[ind-2] && nums[ind-2]==nums[ind-1]||nums[ind - 3] == nums[ind - 2] - 1 && nums[ind - 2] == nums[ind - 1] - 1)
-                && func(nums, ind - 3)){
-                    return true;
+                && func(nums, ind - 3,dp)){
+                    return dp[ind]=true;
                 }
-                return false;
+                return dp[ind]=false;
     }
 }
