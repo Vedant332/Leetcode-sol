250f2209c211f4d527dd25350408cd0ac77562b7
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-30 15:11:46 -0700

    Time: 3 ms (26.01%) | Memory: 41.3 MB (59.53%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -2,7 +2,23 @@ class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int[] dp=new int[days.length+1];
         Arrays.fill(dp,-1);
-        return func(days,costs,0,dp);
+        dp[days.length]=0;
+
+        for(int ind=days.length-1;ind>=0;ind--){
+            int op1=costs[0]+dp[ind+1];
+
+        int i;
+        for(i=ind;i<days.length && days[i]<days[ind]+7;i++){}
+
+        int op2=costs[1]+dp[i];
+
+        for(i=ind;i<days.length && days[i]<days[ind]+30;i++){}
+
+        int op3=costs[2]+dp[i];
+
+         dp[ind]=Math.min(op1,Math.min(op2,op3));
+        }
+        return dp[0];
     }
 
     public int func(int[] days,int[] costs,int ind,int[] dp){
