6f384c5fc79c23c80beb2460f7fc94a876f967b6
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-08-01 16:09:36 -0700

    Time: 149 ms (25.62%) | Memory: 55.2 MB (17.59%) - LeetSync

---
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int[][] arr=new int[scores.length][2];
+
+        for(int i=0;i<scores.length;i++){
+            arr[i][0]=scores[i];
+            arr[i][1]=ages[i];
+        }
+         Arrays.sort(arr, (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+
+         int[][] dp=new int[scores.length+1][scores.length+1];
+         for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+
+        return helper(arr,0,-1,dp);
+    }
+
+    public int helper(int[][] arr,int ind,int prev,int[][] dp){
+        if(ind==arr.length) return 0;
+
+        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+
+        int take=0;
+        if(prev==-1 || arr[ind][0]>=arr[prev][0]){
+             take=arr[ind][0]+helper(arr,ind+1,ind,dp);
+        }
+        int notTake=helper(arr,ind+1,prev,dp);
+
+        return dp[ind][prev+1]=Math.max(take,notTake);
+    }
+}
