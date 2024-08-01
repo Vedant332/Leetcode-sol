9239a81b12ac6f564b22057813d8a225cb7b7613
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-07-31 18:13:53 -0700

    Time: 5 ms (96.70%) | Memory: 107.9 MB (84.40%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,8 +1,20 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         long[] dp=new long[questions.length+1];
-        Arrays.fill(dp,-1);
-        return helper(questions,0,dp);
+        // Arrays.fill(dp,-1);
+
+        dp[questions.length]=0;
+
+        for(int ind=questions.length-1;ind>=0;ind--){
+            long take=questions[ind][0];
+            if(ind+1+questions[ind][1]<questions.length){
+                 take+=dp[ind+1+questions[ind][1]];
+            }
+            long notTake=dp[ind+1];
+
+         dp[ind]=Math.max(take,notTake);
+        }
+        return dp[0];
     }
 
     public long helper(int[][] questions,int ind,long[] dp){
@@ -10,7 +22,7 @@ class Solution {
 
         if(dp[ind]!=-1) return dp[ind];
 
-        long take=questions[ind][0]+helper(questions,ind+1+questions[ind][1],dp);
+        long take=questions[ind][0]+helper(questions,ind+questions[ind][1],dp);
         long notTake=helper(questions,ind+1,dp);
 
         return dp[ind]=Math.max(take,notTake);
