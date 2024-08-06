e74abdd403242620033a1beb4a12d8c22c97a533
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-08-06 16:03:29 -0700

    Time: 9 ms (26.61%) | Memory: 48 MB (25.29%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,10 +2,27 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        // for(int[] row : dp){
+        //     Arrays.fill(row,-1);
+        // }
+        for(int i=0;i<amount+1;i++){
+            if(i % coins[0]==0){
+                dp[0][i]=1;
+            }
         }
-        return helper(amount,coins.length-1,coins,dp);
+
+        for(int ind=1;ind<n;ind++){
+            for(int cost=0;cost<amount+1;cost++){
+                int take=0;
+             if(coins[ind]<=cost){
+            take=dp[ind][cost-coins[ind]];
+        }
+         int notTake=dp[ind-1][cost];
+
+         dp[ind][cost]=take+notTake;
+            }
+        }
+        return dp[n-1][amount];
     }
 
     public int helper(int amount,int ind,int[] coins,int[][] dp){
