d7d586e42f5679fa9eff8c873d22f7dab127f7e1
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-08-08 17:49:31 -0700

    Time: 13 ms (48.98%) | Memory: 41.8 MB (56.38%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,10 +1,24 @@
 class Solution {
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n + 1][target + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
+        // for (int i = 0; i <= n; i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
+
+        dp[0][target]=1;
+        for(int ind=1;ind<n+1;ind++){
+            for(int j=target;j>=0;j--){
+                int count = 0;
+            for(int i=1;i<=k;i++){
+             if (j + i <= target) {
+                count += dp[ind - 1][j + i];
+                count %= 1_000_000_007; 
+             }
+              }
+            dp[ind][j]=count;
+            }
         }
-        return helper(n,k,0,target,dp);
+        return dp[n][0];
     }
 
     public int helper(int n,int k,int sumTillNow,int target,int[][] dp){
@@ -15,10 +29,10 @@ class Solution {
         if(dp[n][sumTillNow]!=-1) return dp[n][sumTillNow];
         int count = 0;
         for(int i=1;i<=k;i++){
-            if (sumTillNow + i <= target) {
+             if (sumTillNow + i <= target) {
                 count += helper(n - 1, k, sumTillNow + i, target, dp);
                 count %= 1_000_000_007; 
-            }
+             }
         }
 
         return dp[n][sumTillNow]=count;
