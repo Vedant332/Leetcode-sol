d2c094db230e8d151a9fade3dea1980d9b8ba2cc
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-08-28 11:06:18 -0700

    Time: 2 ms (89.63%) | Memory: 56 MB (40.27%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,45 +1,27 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        // Arrays.sort(bills);
-
-        // int five=0,ten=0;
-
-        // for(int i=0;i<bills.length;i++){
-        //     if(bills[i]==5){
-        //         five++;
-        //     }else if(bills[i]==10){
-        //         if(five!=0){
-        //             five--;
-        //             ten++;
-        //         }else{
-        //             return false;
-        //         }
-        //     }else{
-        //         if(ten!=0 && five!=0){
-        //             ten--;
-        //             five=five-2;
-        //         }else if(five>=3){
-        //             five=five-3;
-        //         }else{
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return true;
-         int five = 0, ten = 0;
-        for (int i : bills) {
-            if (i == 5)  //if customer gives $5 then we simply take it as lemonade costs $5 
+        int five=0;
+        int ten=0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
                 five++;
-            else if (i == 10) { //if we have $10 then we reduce number of $5 bills by one and increase $10 by one
-                five--; ten++;
+            }else if(bills[i]==10){
+                if(five!=0){
+                    ten++;
+                    five--;
+                }else{
+                    return false;
+                }
+            }else{
+                if(ten!=0 && five!=0){
+                    ten--;
+                    five--;
+                }else if(five>=3){
+                    five=five-3;
+                }else{
+                    return false;
+                }
             }
-            else if (ten > 0 && five>=1) { //if we get $20 then we first try to give back $10+$5
-                ten--; five--;
-            }
-            else        // if we cannot give back $10+$5 then unfortunately we have to give back $5+$5+$5
-                five -= 3;
-            if (five < 0)  // if at any time we do not have any $5 left then we return false
-                return false;
         }
         return true;
     }
