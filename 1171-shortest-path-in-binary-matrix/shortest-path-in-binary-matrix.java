020421f24e667405b54207890b43f89b91c9fa78
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-09-05 11:39:51 -0700

    Time: 18 ms (42.54%) | Memory: 45.6 MB (43.43%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,50 +1,52 @@
-class tuple{
-    int distance;
+class Pair{
     int row;
     int col;
-    tuple(int distance,int row,int col){
-        this.distance=distance;
+    int dist;
+    public Pair(int row,int col,int dist){
         this.row=row;
         this.col=col;
+        this.dist=dist;
     }
 }
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0]==1) return -1;
+          if(grid[0][0]==1) return -1;
         int n=grid.length;
-        int m=grid[0].length;
-         if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
-        if(grid.length==1 && grid[0].length==1 && grid[n-1][m-1]==0) return 1;
-        
-        Queue<tuple> q=new LinkedList<>();
-        int[][] dist=new int[n][m];
-        for(int[] row : dist){
-            Arrays.fill(row, (int)(1e9));
+         if(grid.length==1 && grid[0].length==1 && grid[n-1][n-1]==0) return 1;
+        Queue<Pair> q=new LinkedList<>();
+        int[][] distance=new int[n][n];
+        for(int[] row : distance){
+            Arrays.fill(row,(int) (1e9));
         }
-        dist[0][0]=1;
-        q.add(new tuple(dist[0][0],0,0));
-        while(!q.isEmpty()){
-            int distance=q.peek().distance;
-            int row=q.peek().row;
-            int col=q.peek().col;
-            q.remove();
+        distance[0][0]=1;
 
-            int dx[]={0,1,0,-1,1,-1,-1,1};
+        
+        q.offer(new Pair(0,0,distance[0][0]));
+
+        int dx[]={0,1,0,-1,1,-1,-1,1};
         int dy[]={1,0,-1,0,1,-1,1,-1};
-            for(int i=0;i<8;i++){
-                    int nrow=row+dx[i];
-                    int ncol=col+dy[i];
 
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 
-                    && dist[nrow][ncol]>1+distance){
-                        dist[nrow][ncol]=1+distance;
-                        if(nrow==n-1 && ncol==m-1) return distance+1;
-                        q.add(new tuple(1+distance,nrow,ncol));
+        while(!q.isEmpty()){
+            int r=q.peek().row;
+            int c=q.peek().col;
+            int d=q.peek().dist;
+            q.poll();
+
+            for(int i=0;i<8;i++){
+                int nrow=r+dx[i];
+                int ncol=c+dy[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && distance[nrow][ncol]>1+d && grid[nrow][ncol]==0){
+                    distance[nrow][ncol]=1+d;
+                    if(nrow==n-1 && ncol==n-1){
+                        return 1+d;
                     }
+                    q.offer(new Pair(nrow,ncol,distance[nrow][ncol]));
                 }
             }
-             return -1;
         }
+        return -1;
     }
+}
