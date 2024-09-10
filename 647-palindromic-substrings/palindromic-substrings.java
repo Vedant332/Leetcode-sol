ea0d252290d8833539a726128891c30e9b05d048
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-09-10 11:09:46 -0700

    Time: 21 ms (30.24%) | Memory: 45.1 MB (31.49%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,22 +1,26 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n=s.length();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(helper(s,i,j)){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+       int n=s.length();
+       boolean[][] dp=new boolean[n][n];
+       int count=0;
 
-    public boolean helper(String s,int i,int j){
-        if(i>j) return true;
-        if(s.charAt(i)==s.charAt(j)){
-            return helper(s,i+1,j-1);
+       for(int L=1;L<n+1;L++){
+        for(int i=0;i+L-1<n ;i++){
+           int j=i+L-1; 
+
+           if(i==j){
+            dp[i][j]=true;
+           }else if(i+1==j){
+            dp[i][j]=(s.charAt(i)==s.charAt(j));
+           }else{
+            dp[i][j]=(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true);
+           }
+
+           if(dp[i][j]==true){
+            count++;
+           }
         }
-        return false;
+       }
+       return count;
     }
 }
