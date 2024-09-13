f6720d34fcdf68a1e4cc723fdf5ca1b389dea4be
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-09-13 12:09:07 -0700

    Time: 15 ms (88.24%) | Memory: 41.6 MB (99.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,41 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.length()==1) return s;
-        String[][] dp=new String[s.length()][s.length()];
-
-        for(String[] row: dp){
-            Arrays.fill(row,null);
+      int start=0;
+      int end=0;
+      for(int i=0;i<s.length();i++){
+        int len1=func(s,i,i);
+        int len2=func(s,i,i+1);
+        int len=Math.max(len1,len2);
+        if(len>end-start){
+            start=i-(len-1)/2;
+            end=i+len/2;
         }
-
-        return func(s,0,s.length()-1,dp);
+      }
+      return s.substring(start,end+1);
     }
 
-    public String func(String s,int start,int end,String[][] dp){
-        if(start>end){
-            return "";
+    public int func(String s,int start,int end){
+        while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end)){
+            start--;
+            end++;
         }
-
-        if(s.charAt(start)==s.charAt(end) && isPalindrome(s,start+1,end-1)){
-            return s.substring(start,end+1);
-        }
-
-        if(dp[start][end]!=null) return dp[start][end];
-
-        String right=func(s,start+1,end,dp);
-        String left=func(s,start,end-1,dp);
-
-        return dp[start][end]=right.length() > left.length()?right:left;
-    }
-
-    public boolean isPalindrome(String s,int start,int end){
-        while(start<end){
-            if(s.charAt(start)==s.charAt(end)){
-                start++;
-                end--;
-            }else{
-                return false;
-            }
-        }
-        return true;
+        return end-start-1;
     }
 }
