dbe1ed5c3ce48fdb20c9360453a0cd6e42909f84
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-09-18 14:29:35 -0700

    Time: 3 ms (96.31%) | Memory: 45.1 MB (34.10%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,23 +1,20 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m=word1.length();
-        int n=word2.length();
-        int[][] dp=new int[m][n];
+        int[][] dp=new int[word1.length()][word2.length()];
         for(int[] row : dp){
             Arrays.fill(row,-1);
         }
-        return helper(word1,word2,m-1,n-1,dp);
+        return helper(word1.length()-1,word2.length()-1,word1,word2,dp);
     }
 
-    public int helper(String word1, String word2,int i,int j,int[][] dp){
+    public int helper(int i,int j,String word1,String word2,int[][] dp){
         if(i<0) return j+1;
         if(j<0) return i+1;
-
         if(dp[i][j]!=-1) return dp[i][j];
         if(word1.charAt(i)==word2.charAt(j)){
-            return dp[i][j]=0+helper(word1,word2,i-1,j-1,dp);
+            return dp[i][j]=0+ helper(i-1,j-1,word1,word2,dp);
         }
 
-        return dp[i][j]=1+Math.min(helper(word1,word2,i-1,j-1,dp),Math.min(helper(word1,word2,i-1,j,dp),helper(word1,word2,i,j-1,dp)));
+        return dp[i][j]=1+Math.min(helper(i-1,j-1,word1,word2,dp),Math.min(helper(i-1,j,word1,word2,dp),helper(i,j-1,word1,word2,dp)));
     }
 }
