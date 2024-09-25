927563470601328a23cfbc68549076ac22f807ca
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-09-25 14:56:50 -0700

    Time: 1 ms (63.19%) | Memory: 45.2 MB (20.76%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,26 +1,59 @@
+class Pair{
+    int node;
+    int color;
+    public Pair(int node,int color){
+        this.node=node;
+        this.color=color;
+    }
+}
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int m=graph.length;
-        int n=graph[0].length;
         int[] color=new int[m];
         Arrays.fill(color,-1);
-
+        
         for(int i=0;i<m;i++){
             if(color[i]==-1){
-                if(dfs(i,graph,color,0)==false) return false;
+                if(!bfs(graph,i,color,0)) return false;
             }
         }
         return true;
     }
 
-    public boolean dfs(int node,int[][] graph,int[] color,int prevNode){
-        color[node]=prevNode;
+    public boolean bfs(int[][] graph,int node,int[] color,int colour){
+        Queue<Pair> q=new LinkedList<>();
+        color[node]=colour;
+        q.offer(new Pair(node,colour));
+
+        while(!q.isEmpty()){
+            int ele=q.peek().node;
+            int col=q.peek().color;
+            q.poll();
+
+            for(int it : graph[ele]){
+                if(color[it]==-1){
+                    color[it]=1-col;
+                    q.offer(new Pair(it,1-col));
+                }else{
+                    if(color[it]==col){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(int[][] graph,int node,int[] color,int colour){
+        color[node]=colour;
 
         for(int it : graph[node]){
             if(color[it]==-1){
-                if(dfs(it,graph,color,1-prevNode)==false) return false;
-            }else if(color[it]==prevNode){
-                return false;
+                if(!dfs(graph,it,color,1-colour)) return false;
+            }else{
+                if(color[it]==colour){
+                    return false;
+                }
             }
         }
         return true;
