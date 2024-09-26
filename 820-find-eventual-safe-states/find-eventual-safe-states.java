7e9e12c8e46b0aa931bae84094e23dc81f8195e5
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-09-26 15:41:43 -0700

    Time: 4 ms (98.85%) | Memory: 55.5 MB (30.86%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,37 +1,39 @@
 class Solution {
-    public boolean dfs(int node,int[] vis,int[] pathvis,int[] check,int[][] graph) {
-       vis[node]=1;
-        pathvis[node]=1;
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+       int m=graph.length;
+       int[] vis=new int[m];
+       int[] pathVis=new int[m];
+       int[] check=new int[m];
+       List<Integer> ls=new ArrayList<>();
+
+       for(int i=0;i<m;i++){
+        if(vis[i]==0){
+            dfs(graph,i,vis,pathVis,check);
+        }
+       }
+
+       for(int i=0;i<m;i++){
+        if(check[i]==1){
+            ls.add(i);
+        }
+       }
+       return ls;
+    }
+
+    public boolean dfs(int[][] graph,int node,int[] vis,int[] pathVis,int[] check){
+        vis[node]=1;
+        pathVis[node]=1;
         check[node]=0;
-        for(Integer it : graph[node]){
-            if(vis[it]!=1){
-                if(dfs(it,vis,pathvis,check,graph)==true) return true;
-            }else if(pathvis[it]==1){
-                return true;
+
+        for(int it : graph[node]){
+            if(vis[it]==0){
+                if(dfs(graph,it,vis,pathVis,check)) return true;
+            }else{
+                if(pathVis[it]==1) return true;
             }
         }
         check[node]=1;
-        pathvis[node]=0;
+        pathVis[node]=0;
         return false;
-    }
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        int m=graph[0].length;
-        int[] vis=new int[n];
-        int[] pathvis=new int[n];
-        int[] check=new int[n];
-
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(i,vis,pathvis,check,graph);
-            }
-        }
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(check[i]==1){
-                ans.add(i);
-            }
-        }
-        return ans;
     }
 }
