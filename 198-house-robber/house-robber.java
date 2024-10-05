aefbb308cca71c7f06469124b121196333732616
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-05 12:53:27 -0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (97.92%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,34 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        
-        for (int i = 2; i < nums.length; i++) {
-            int take = nums[i] + dp[i - 2];
-            int notTake = dp[i - 1];
-            dp[i] = Math.max(take, notTake);
-        }
-        return dp[nums.length - 1];
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+
+        return helper(nums,nums.length-1,dp);
     }
 
-    public int func(int ind,int[] nums,int[] dp){
-        if(ind<=0){
-            if(ind<0){
-                return 0;
-            }
-            return nums[0];
-        }
-
+    public int helper(int[] nums,int ind,int[] dp){
+        if(ind==0) return nums[ind];
         if(dp[ind]!=-1) return dp[ind];
-
-        int take=nums[ind]+func(ind-2,nums,dp);
-        int notTake=func(ind-1,nums,dp);
-
+        int notTake=helper(nums,ind-1,dp);
+        int take=nums[ind];
+        if(ind>1){
+            take+=helper(nums,ind-2,dp);
+        }
         return dp[ind]=Math.max(take,notTake);
-
     }
 }
