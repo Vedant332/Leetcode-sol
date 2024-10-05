7058342893e5f327aa46801d6b964e1f6b2f2e02
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-05 13:59:28 -0700

    Time: 0 ms (100.00%) | Memory: 42.1 MB (12.78%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,39 +1,25 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int i=obstacleGrid.length;
-        int j=obstacleGrid[0].length;
-
-        int[][] dp=new int[i][j];
-
-        for(int m=0;m<i;m++){
-            for(int n=0;n<j;n++){
-                if(obstacleGrid[m][n]==1){
-                    dp[m][n]=0;
-                }
-                else if(m==0 && n==0){
-                    dp[m][n]=1;
-                }else{
-                    int up=0;
-                    int left=0;
-                    if(m>0) up=dp[m-1][n];
-                    if(n>0) left=dp[m][n-1];
-                  dp[m][n]=up+left;
-                }
-            }
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+       if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
+        int[][] dp=new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
 
-        return dp[i-1][j-1];
+        return func(obstacleGrid,m-1,n-1,dp);
     }
 
-    public int func(int m,int n,int[][] obstacleGrid){
-        if(m>=0 && n>=0 && obstacleGrid[m][n]==1) return 0;
-        if(m==0 && n==0) return 1;
+    public int func(int[][] obstacleGrid,int i,int j,int[][] dp){
+        if(i<0 || j<0 ) return 0;
+        if(i==0 && j==0) return 1;
+        if(obstacleGrid[i][j]==1) return 0;
 
-        if(m<0 || n<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=func(obstacleGrid,i-1,j,dp);
+        int left=func(obstacleGrid,i,j-1,dp);
 
-        int up=func(m-1,n,obstacleGrid);
-        int left=func(m,n-1,obstacleGrid);
-
-        return up+left;
+        return dp[i][j]=up+left;
     }
 }
