539dcdc0bc5bf6833644cc78c16fac26052e9709
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-06 11:50:22 -0700

    Time: 1 ms (97.95%) | Memory: 47.4 MB (39.42%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,31 +3,13 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int[][] dp=new int[m][n];
-
-        
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[0][0]=grid[0][0];
-                }else{
-                    int up = grid[i][j];
-                    if(i>0) up+=dp[i - 1][j];
-                    else up+=9999;
-
-                    int left = grid[i][j];
-                    if(j>0) left+=dp[i][j - 1];
-                    else left+=9999;
-                    dp[i][j] =   Math.min(up, left);
-                }
-            }
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        
-        return dp[m-1][n-1];
+        return helper(grid,m-1,n-1,dp);
     }
-
     public int helper(int[][] arr,int i,int j,int[][] dp){
-         if (i < 0 || j < 0) return Integer.MAX_VALUE; 
+        if (i < 0 || j < 0) return Integer.MAX_VALUE; 
 
         if (i == 0 && j == 0) return arr[i][j];
 
