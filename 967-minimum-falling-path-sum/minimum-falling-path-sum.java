11be60eceb1b8a1749dd254e99e74d288820f5da
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-06 12:25:47 -0700

    Time: 7 ms (56.24%) | Memory: 45 MB (85.28%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,45 +1,47 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-       int m=matrix.length;
+        int m=matrix.length;
         int n=matrix[0].length;
         int[][] dp=new int[m][n];
-         for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             dp[0][i]=matrix[0][i];
         }
         for(int i=1;i<m;i++){
             for(int j=0;j<n;j++){
-
-                int u=matrix[i][j]+dp[i-1][j];
-
-                int ld=matrix[i][j];
-                if(j-1>=0) ld+=dp[i-1][j-1];
-                else ld+=10000;
-
-                int rd=matrix[i][j];
-                if(j+1<n) rd+=dp[i-1][j+1];
-                else rd+=10000;
-
-                dp[i][j]=Math.min(ld,Math.min(u,rd));
+                int up=dp[i-1][j];
+                int upleft=0;
+                if(j-1>=0){
+                    upleft=dp[i-1][j-1];
+                }else{
+                    upleft=10000;
+                }
+                int upright=0;
+                if(j+1<n){
+                    upright=dp[i-1][j+1];
+                }else{
+                    upright=10000;
+                }
+                dp[i][j]=matrix[i][j]+Math.min(up,Math.min(upleft,upright));
             }
         }
         int mini=10000;
-        for(int j=0;j<n;j++){
-            mini=Math.min(mini,dp[m-1][j]);
+        for(int i=0;i<n;i++){
+            mini=Math.min(mini,dp[m-1][i]);
         }
         return mini;
     }
 
-    public static int helper(int i,int j,int[][] arr,int m,int n,int[][] dp){
-        if(j<0 || j>=n) return 10000;
-
+    public int helper(int[][] arr,int i,int j,int[][] dp){
+        if(j<0 || j>=arr[0].length) return 10000;
         if(i==0) return arr[i][j];
 
         if(dp[i][j]!=-1) return dp[i][j];
+        int up=helper(arr,i-1,j,dp);
+        int upleft=helper(arr,i-1,j-1,dp);
+        int upright=helper(arr,i-1,j+1,dp);
 
-        int ld=arr[i][j]+helper(i-1,j,arr,m,n,dp);
-        int u=arr[i][j]+helper(i-1,j-1,arr,m,n,dp);
-        int rd=arr[i][j]+helper(i-1,j+1,arr,m,n,dp);
+        dp[i][j]=arr[i][j]+Math.min(up,Math.min(upleft,upright));
 
-        return dp[i][j]=Math.min(ld,Math.min(u,rd));
+        return dp[i][j];
     }
 }
