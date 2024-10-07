195a3276c17e596f92dda30dbdc0b0e6c8f97639
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-07 16:42:21 -0700

    Time: 112 ms (13.56%) | Memory: 72.3 MB (8.16%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -4,11 +4,34 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             totSum+=nums[i];
         }
+        Boolean[][] dp=new Boolean[nums.length][totSum+1];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <= totSum/2; j++) {
+                dp[i][j] = false;  
+            }
+        }
         if(totSum%2!=0) return false;
 
-        Boolean[][] dp=new Boolean[nums.length][totSum+1];
+        for(int i=0;i<nums.length;i++){
+            dp[i][0]=true;
+        }
+        if (nums[0] <= totSum/2) {
+              dp[0][nums[0]] = true;  
+        }
 
-        return helper(nums,nums.length-1,totSum/2,dp);
+        for(int ind=1;ind<nums.length;ind++){
+            for(int sum=1;sum<=totSum/2;sum++){
+                Boolean notTake=dp[ind-1][sum];
+                Boolean take=false;
+                if(nums[ind]<=sum){
+                    take=dp[ind-1][sum-nums[ind]];
+                }
+                 dp[ind][sum]=take || notTake;
+            }
+        }
+
+
+        return dp[nums.length-1][totSum/2];
     }
 
     public boolean helper(int[] nums,int ind,int sum,Boolean[][] dp){
