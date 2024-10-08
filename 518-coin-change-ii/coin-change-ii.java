4ddb7f1060dbcba45d4c8e57b7a5cfcc8f4d819f
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-08 14:01:18 -0700

    Time: 11 ms (26.02%) | Memory: 49.2 MB (10.62%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,46 +2,28 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        // for(int[] row : dp){
-        //     Arrays.fill(row,-1);
-        // }
-        for(int i=0;i<amount+1;i++){
-            if(i % coins[0]==0){
-                dp[0][i]=1;
-            }
+        for(int[] row: dp){
+            Arrays.fill(row,-1);
         }
-
-        for(int ind=1;ind<n;ind++){
-            for(int cost=0;cost<amount+1;cost++){
-                int take=0;
-             if(coins[ind]<=cost){
-            take=dp[ind][cost-coins[ind]];
-        }
-         int notTake=dp[ind-1][cost];
-
-         dp[ind][cost]=take+notTake;
-            }
-        }
-        return dp[n-1][amount];
+        int ans=func(coins,n-1,amount,dp);
+        
+        return ans;
     }
 
-    public int helper(int amount,int ind,int[] coins,int[][] dp){
-        if(ind==0){
-            if(amount%coins[ind]==0){
-                return 1;
-            }else{
-                return 0;
-            }
+    public int func(int[] coins,int ind,int cost,int[][] dp){
+         if (ind == 0) {
+            if (cost % coins[ind] == 0) return 1;
+            return 0;  
         }
+       
 
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-
+        if(dp[ind][cost]!=-1) return dp[ind][cost];
+        int notTake=func(coins,ind-1,cost,dp);
         int take=0;
-        if(coins[ind]<=amount){
-            take=helper(amount-coins[ind],ind,coins,dp);
+        if(coins[ind]<=cost){
+            take=func(coins,ind,cost-coins[ind],dp);
         }
-        int notTake=helper(amount,ind-1,coins,dp);
 
-        return dp[ind][amount]=take+notTake;
+        return dp[ind][cost]=take+notTake;
     }
 }
