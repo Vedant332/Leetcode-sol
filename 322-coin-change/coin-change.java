2622bbe90e392b08939ad181343a80d6b0ceee0b
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-08 13:34:15 -0700

    Time: 27 ms (28.97%) | Memory: 44.9 MB (29.35%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,10 +2,25 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+            }else{
+                dp[0][i]=(int) Math.pow(10, 9);
+            }
         }
-        int ans=helper(coins,n-1,amount,dp);
+
+        for(int ind=1;ind<n;ind++){
+            for(int am=0;am<=amount;am++){
+                int notTake=dp[ind-1][am];
+                 int take=Integer.MAX_VALUE;
+                if(coins[ind]<=am){
+                    take=1+dp[ind][am-coins[ind]];
+                }
+                 dp[ind][am]=Math.min(take,notTake);
+            }
+        }
+        int ans=dp[n-1][amount];
         if (ans >= (int) Math.pow(10, 9))
             return -1;
         return ans;
