05244ce9180f126c6e941c640e8f37bc408e8501
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-08 12:55:42 -0700

    Time: 39 ms (14.53%) | Memory: 45.2 MB (21.76%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,42 +1,29 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-       int n = coins.length;
-         int[][] dp = new int[n][amount + 1];
-        for(int i=0;i<amount+1;i++){
-           if (i % coins[0] == 0)
-                dp[0][i]=i / coins[0];
-            else
-                dp[0][i]=(int) Math.pow(10, 9);
+        int n=coins.length;
+        int[][] dp=new int[n][amount+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        for(int ind=1;ind<n;ind++){
-            for(int j=0;j<amount+1;j++){
-                int take= (int) Math.pow(10, 9);
-        if(coins[ind]<=j){
-            take=1+dp[ind][j-coins[ind]];
-        }
-        int notTake=dp[ind-1][j];
-
-         dp[ind][j]=Math.min(take,notTake);
-            }
-        }
-        return dp[n - 1][amount] >= (int) Math.pow(10, 9) ? -1 : dp[n - 1][amount];
+        int ans=helper(coins,n-1,amount,dp);
+        if (ans >= (int) Math.pow(10, 9))
+            return -1;
+        return ans;
     }
 
-     public int solve(int[] coins,int ind,int amount,int[][] dp){
-         if (ind == 0) {
-            if (amount % coins[0] == 0)
-                return amount / coins[0];
-            else
-                return (int) Math.pow(10, 9);
+    public int helper(int[] coins,int ind,int amount,int[][] dp){
+        if(ind==0){
+            if(amount%coins[ind]==0) return amount/coins[ind];
+            else return (int) Math.pow(10, 9);
         }
+        
 
         if(dp[ind][amount]!=-1) return dp[ind][amount];
-
-        int take= Integer.MAX_VALUE;
+        int notTake=helper(coins,ind-1,amount,dp);
+        int take=Integer.MAX_VALUE;
         if(coins[ind]<=amount){
-            take=1+solve(coins,ind,amount-coins[ind],dp);
+            take=1+helper(coins,ind,amount-coins[ind],dp);
         }
-        int notTake=solve(coins,ind-1,amount,dp);
 
         return dp[ind][amount]=Math.min(take,notTake);
     }
