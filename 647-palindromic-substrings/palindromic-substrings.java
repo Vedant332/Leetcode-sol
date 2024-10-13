2d9d4483f3a4cf8afee8b6cf999fce9de014d3a1
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-12 21:03:01 -0700

    Time: 24 ms (28.06%) | Memory: 45.3 MB (19.36%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,26 +1,28 @@
 class Solution {
     public int countSubstrings(String s) {
-       int n=s.length();
-       boolean[][] dp=new boolean[n][n];
-       int count=0;
-
-       for(int L=1;L<n+1;L++){
-        for(int i=0;i+L-1<n ;i++){
-           int j=i+L-1; 
-
-           if(i==j){
-            dp[i][j]=true;
-           }else if(i+1==j){
-            dp[i][j]=(s.charAt(i)==s.charAt(j));
-           }else{
-            dp[i][j]=(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true);
-           }
-
-           if(dp[i][j]==true){
-            count++;
-           }
+        int n=s.length();
+        boolean[][] dp=new boolean[n][n];
+        for(boolean[] row : dp){
+            Arrays.fill(row,false);
         }
-       }
-       return count;
+        int count=0;
+        for(int L=1;L<=n;L++){
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+
+                if(i==j){
+                    dp[i][j]=true;
+                }else if(i+1==j){
+                    if(s.charAt(i)==s.charAt(j)) dp[i][j]=true;
+                }else{
+                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]) dp[i][j]=true;
+                }
+
+                if(dp[i][j]==true){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
