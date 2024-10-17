0c717fdd5c3300df12c719f6873d277f700e8e50
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-17 15:56:07 -0700

    Time: 4 ms (73.86%) | Memory: 45.1 MB (34.33%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,34 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp=new int[word1.length()+1][word2.length()+1];
-        
-        for(int i=0;i<word1.length()+1;i++){
-            dp[i][0]=i;
+        int m=word1.length();
+        int n=word2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        for(int j=0;j<word2.length()+1;j++){
-            dp[0][j]=j;
-        }
-
-        for(int i=1;i<word1.length()+1;i++){
-            for(int j=1;j<word2.length()+1;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                dp[i][j]=dp[i-1][j-1];
-             }else{
-                dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
-                }
-            }
-        }
-        return dp[word1.length()][word2.length()];
+        return func(m,n,word1,word2,dp);
     }
 
-    public int helper(int i,int j,String word1,String word2,int[][] dp){
+    public int func(int i,int j,String s, String t,int[][] dp){
         if(i==0) return j;
         if(j==0) return i;
+
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1.charAt(i-1)==word2.charAt(j-1)){
-            return dp[i][j]=0+ helper(i-1,j-1,word1,word2,dp);
+
+        if(s.charAt(i-1)==t.charAt(j-1)){
+            return func(i-1,j-1,s,t,dp);
         }
 
-        return dp[i][j]=1+Math.min(helper(i-1,j-1,word1,word2,dp),Math.min(helper(i-1,j,word1,word2,dp),helper(i,j-1,word1,word2,dp)));
+        return dp[i][j]=1+Math.min(func(i-1,j-1,s,t,dp),Math.min(func(i-1,j,s,t,dp),func(i,j-1,s,t,dp)));
     }
 }
