e10d3f626d3cc423ccdd63aa281d35440faa6649
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-26 13:32:11 -0700

    Time: 6 ms (92.66%) | Memory: 41.7 MB (87.85%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,38 +1,24 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int[] dp=new int[arr.length+1];
+        int n=arr.length;
+        int[] dp=new int[n];
         Arrays.fill(dp,-1);
-
-        dp[arr.length]=0;
-
-        for(int ind=arr.length-1;ind>=0;ind--){
-            int sum=0;
-        int maxSum=0;
-        int maxEle=Integer.MIN_VALUE;
-        for(int i=0;i<Math.min(k,arr.length-ind);i++){
-            maxEle=Math.max(maxEle,arr[ind+i]);
-            sum=maxEle*(i+1) + dp[ind+i+1];
-            maxSum=Math.max(sum,maxSum);
-        }
-        dp[ind]=maxSum;
-        }
-        
-        return dp[0];
+        return func(0,arr,k,n,dp);
     }
 
-    // public int func(int[] arr,int k, int ind,int[] dp){
-    //     if(ind>=arr.length) return 0;
+    public int func(int ind,int[] arr,int k,int n,int[] dp){
+        if(ind==n) return 0;
 
-    //      if (dp[ind] != -1) return dp[ind];
-
-    //     int sum=0;
-    //     int maxSum=0;
-    //     int maxEle=Integer.MIN_VALUE;
-    //     for(int i=0;i<Math.min(k,arr.length-ind);i++){
-    //         maxEle=Math.max(maxEle,arr[ind+i]);
-    //         sum=maxEle*(i+1) + func(arr,k,ind+i+1,dp);
-    //         maxSum=Math.max(sum,maxSum);
-    //     }
-    //     return dp[ind]=maxSum;
-    // }
+        if(dp[ind]!=-1) return dp[ind];
+        int len=0;
+        int maxi=Integer.MIN_VALUE;
+        int maxAs=0;
+        for(int i=ind;i<Math.min(n,ind+k);i++){
+            len++;
+            maxi=Math.max(maxi,arr[i]);
+            int sum=len*maxi+func(i+1,arr,k,n,dp);
+            maxAs=Math.max(maxAs,sum);
+        }
+        return dp[ind]=maxAs;
+    }
 }
