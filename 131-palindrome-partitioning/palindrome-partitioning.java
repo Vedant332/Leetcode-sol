5d0c16041745cfdcdd403187a3fe2ed09e47f153
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-10-26 12:32:50 -0700

    Time: 8 ms (57.16%) | Memory: 56.7 MB (66.76%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,25 +1,26 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans=new ArrayList<>();
-        helper(s,0,new ArrayList<>(),ans);
+     List<List<String>> ans =new ArrayList<>();
+        func(0,s,new ArrayList<>(),ans);
         return ans;
     }
 
-    public void helper(String s,int ind,List<String> res,List<List<String>> ans){
+    public void func(int ind,String s,List<String> res,List<List<String>> ans){
         if(ind==s.length()){
             ans.add(new ArrayList<>(res));
             return;
         }
+
         for(int i=ind;i<s.length();i++){
             if(isPalin(ind,i,s)){
                 res.add(s.substring(ind,i+1));
-                helper(s,i+1,res,ans);
+                func(i+1,s,res,ans);
                 res.remove(res.size()-1);
             }
         }
     }
-     public boolean isPalin(int start, int end, String s){
-        
+
+    public boolean isPalin(int start,int end,String s){
         while(start<end){
             if(s.charAt(start)==s.charAt(end)){
                 start++;
@@ -29,5 +30,5 @@ class Solution {
             }
         }
         return true;
-}
+    }
 }
