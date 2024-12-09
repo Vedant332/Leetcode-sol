beee68e5f789a479e7a23b2f06569b16a2d674f7
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2024-12-09 13:01:59 -0800

    Time: 28 ms (34.60%) | Memory: 65.2 MB (11.20%) - LeetSync

---
+++ 2045-cutting-ribbons/cutting-ribbons.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxLength(int[] ribbons, int k) {
+        int maxEle=Integer.MIN_VALUE;
+        for(int i=0;i<ribbons.length;i++){
+            maxEle=Math.max(maxEle,ribbons[i]);
+        }
+        int lo=1;
+        int hi=maxEle;
+        int ans=0;
+
+        while(lo<=hi){
+         int mid=lo+(hi-lo)/2;
+            if(check(ribbons,mid,k)){
+                lo=mid+1;
+                ans=Math.max(ans,mid);
+            }else{
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean check(int[] ribbons,int mid,int k){
+        int splitRibbon=0;
+        for(int i=0;i<ribbons.length;i++){
+            int NosplitRibbon=ribbons[i]/mid;
+            if(NosplitRibbon>0){
+                splitRibbon+= NosplitRibbon;
+                if(splitRibbon>=k) return true;
+            }
+        }
+            return false;
+    }
+}
