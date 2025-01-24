f5dce6f6a4fc19e06277ae39cd1137dffa07fd0e
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-01-24 15:26:03 -0800

    Time: 3 ms (10.04%) | Memory: 48.9 MB (5.06%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,45 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int[][] dp=new int[n+1][2];
-        dp[n][0]=0;
-        dp[n][1]=0;
-
-        for(int ind=n-1;ind>=0;ind--){
-            for(int flag=1;flag>=0;flag--){
-                int profit=0;
-                if(flag==0){
-            int buy=-prices[ind]+dp[ind+1][1-flag];
-            int notBuy=dp[ind+1][flag];
-            profit=Math.max(buy,notBuy);
-        }else{
-            int sell=prices[ind]+dp[ind+1][1-flag];
-            int notSell=dp[ind+1][flag];
-            profit=Math.max(sell,notSell);
+        int[][] dp=new int[prices.length][2];
+        for(int[] row : dp ){
+            Arrays.fill(row,-1);
         }
-         dp[ind][flag]=profit;
-            }
-        }
-        
-        return dp[0][0];
+        return helper(0,prices,0,dp);
     }
 
-    public int helper(int ind,int flag,int[] prices,int[][] dp){
+    public int helper(int ind,int[] prices,int flag,int[][] dp){
         if(ind==prices.length) return 0;
-
-        int profit=0;
-
         if(dp[ind][flag]!=-1) return dp[ind][flag];
+        int profit=0;
         if(flag==0){
-            int buy=-prices[ind]+helper(ind+1,1-flag,prices,dp);
-            int notBuy=helper(ind+1,flag,prices,dp);
+            int buy=-prices[ind]+helper(ind+1,prices,1-flag,dp);
+            int notBuy=helper(ind+1,prices,flag,dp);
             profit=Math.max(buy,notBuy);
         }else{
-            int sell=prices[ind]+helper(ind+1,1-flag,prices,dp);
-            int notSell=helper(ind+1,flag,prices,dp);
+            int sell=prices[ind]+helper(ind+1,prices,1-flag,dp);
+            int notSell=helper(ind+1,prices,flag,dp);
             profit=Math.max(sell,notSell);
         }
-
         return dp[ind][flag]=profit;
     }
 }
