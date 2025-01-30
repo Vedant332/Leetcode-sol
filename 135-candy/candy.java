b4657bc10cc9b21e249f931eb1b1cb0c4fb2fa00
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-01-29 16:36:27 -0800

    Time: 3 ms (83.23%) | Memory: 46.3 MB (12.61%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,40 +1,38 @@
 class Solution {
-    public int candy(int[] ratings) {
-          int i =1;
-        int sum =1;
-        int n = ratings.length-1;
-        while(i<=n)
-        {
-           int peek =1;
-            while( i<=n &&  ratings[i]>ratings[i-1]  )
-            {
-                peek = peek+1;
-                sum  +=  peek;
-                i++;
-               
-            }
-            int down = 0;
-            while( i<=n && ratings[i] < ratings[i-1] )
-            {
-                if(i==1||ratings[i-1]==ratings[i-2])
-                down=1;
+    public int candy(int[] r) {
+         int n = r.length;
+        if (n == 1) return 1; 
 
-                down=down+1;
-                sum += down;
+        int ans = n; 
+        int i = 1;
+
+        while (i < n) {
+            
+            if (r[i] == r[i - 1]) {
+                i++;
+                continue;
+            }
+
+          
+            int peak = 0; 
+            while (i < n && r[i] > r[i - 1]) {
+                peak++;
+                ans += peak;
                 i++;
             }
-            while(  i<=n   &&  ratings[i]==ratings[i-1])
-            {
-                sum += 1;
-                ++i;
-            }
-            if(down>=peek && peek!=1)
-            {
-                sum += down-peek+1;
-            }
+
             
+            int down = 0; 
+            while (i < n && r[i] < r[i - 1]) {
+                down++;
+                ans += down;
+                i++;
+            }
+
+           
+            ans -= Math.min(peak, down); 
         }
-       
-       return sum;
+
+        return ans;
     }
 }
