db74981138fb246ee19a9db7bb39ab3144bc3778
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-02-10 12:21:29 -0800

    Time: 0 ms (100.00%) | Memory: 41.8 MB (15.85%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans=new ArrayList<>();
+        
+        int rowBegin=0;
+        int rowEnd=matrix.length-1;
+        int colBegin=0;
+        int colEnd=matrix[0].length-1;
+
+        while(rowBegin<=rowEnd && colBegin<=colEnd){
+            for(int i=colBegin;i<=colEnd;i++){
+                ans.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+
+            for(int j=rowBegin;j<=rowEnd;j++){
+                ans.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+           if(rowBegin<=rowEnd){
+             for(int i=colEnd;i>=colBegin;i--){
+                ans.add(matrix[rowEnd][i]);
+            }
+            rowEnd--;
+            }
+
+            if(colBegin<=colEnd){
+                for(int j=rowEnd;j>=rowBegin;j--){
+                ans.add(matrix[j][colBegin]);
+            }
+            colBegin++;
+            }
+        }
+        return ans;
+    }
+}
