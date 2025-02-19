70dfcfa5d46abffb61ebe193d5a0df37565e709d
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-02-19 15:16:59 -0800

    Time: 7 ms (29.54%) | Memory: 44.6 MB (79.76%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,32 +1,32 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> st=new Stack<>();
-        int ans=0;
-        for(int i=0;i<tokens.length;i++){
-            if(!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("*") && !tokens[i].equals("/")){
-                 String s=tokens[i];
-                 Integer val=Integer.valueOf(s);
-                 st.push(val);
+        int n=tokens.length;
+        for(int i=0;i<n;i++){
+            if(!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("/") && !tokens[i].equals("*")){
+                String s=tokens[i];
+                Integer val=Integer.valueOf(s);
+                st.push(val);
             }else if(!st.isEmpty() && tokens[i].equals("+")){
-                int a=st.pop();
-                int b=st.pop();
-                 ans=a+b;
-                 st.push(ans);
+                int sec=st.pop();
+                int fir=st.pop();
+                int ans=fir+sec;
+                st.push(ans);
             }else if(!st.isEmpty() && tokens[i].equals("-")){
-                int a=st.pop();
-                int b=st.pop();
-                 ans=b-a;
-                 st.push(ans);
+                int sec=st.pop();
+                int fir=st.pop();
+                int ans=fir-sec;
+                st.push(ans);
             }else if(!st.isEmpty() && tokens[i].equals("*")){
-                int a=st.pop();
-                int b=st.pop();
-                 ans=b*a;
-                 st.push(ans);
+                int sec=st.pop();
+                int fir=st.pop();
+                int ans=fir*sec;
+                st.push(ans);
             }else if(!st.isEmpty() && tokens[i].equals("/")){
-                int a=st.pop();
-                int b=st.pop();
-                 ans=b/a;
-                 st.push(ans);
+                int sec=st.pop();
+                int fir=st.pop();
+                int ans=fir/sec;
+                st.push(ans);
             }
         }
         return st.peek();
