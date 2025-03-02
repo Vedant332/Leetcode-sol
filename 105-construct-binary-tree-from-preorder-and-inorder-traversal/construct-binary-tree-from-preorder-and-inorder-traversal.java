7323d5d3c8593e7387da1d5ebf3b83718b2ef3e6
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-03-01 16:16:32 -0800

    Time: 2 ms (65.41%) | Memory: 44.4 MB (62.43%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,22 +15,22 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-
-        TreeNode root=buildTreeHelper(preorder,0,preorder.length-1,inorder,0,inorder.length-1,map);
+        TreeNode root=func(preorder,0,preorder.length-1,inorder,0,inorder.length-1,map);
         return root;
     }
-    public TreeNode buildTreeHelper(int[] preorder,int ps,int pe,int[] inorder,int is,int ie, Map<Integer,Integer> map){
+    public TreeNode func(int[] preorder,int ps,int pe,int[] inorder,int is,int ie,HashMap<Integer,Integer> map){
         if(ps>pe || is>ie) return null;
-        TreeNode root=new TreeNode(preorder[ps]);
-        int inRoot=map.get(root.val);
-        int numsLeft=inRoot-is;
 
-        root.left=buildTreeHelper(preorder,ps+1,ps+numsLeft,inorder,is,inRoot-1,map);
-        root.right=buildTreeHelper(preorder,ps+numsLeft+1,pe,inorder,inRoot+1,ie,map);
+        TreeNode root=new TreeNode(preorder[ps]);
+        int rootIndex=map.get(root.val);
+        int numsLeft=rootIndex-is;
+
+        root.left=func(preorder,ps+1,ps+numsLeft,inorder,is,rootIndex-1,map);
+        root.right=func(preorder,ps+numsLeft+1,pe,inorder,rootIndex+1,ie,map);
 
         return root;
     }
