3ee6b834cab54bcdd35a3e00b0627a9157c6a69f
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-03-06 13:41:38 -0800

    Time: 5 ms (34.40%) | Memory: 52 MB (23.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -4,30 +4,29 @@ class Solution {
         int n=grid[0].length;
         int count=0;
         int[][] visited=new int[m][n];
-
+        for(int[] row : visited){
+            Arrays.fill(row,0);
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j]==0 && grid[i][j]=='1'){
-                    dfs(i,j,grid,visited,m,n);
+                if(grid[i][j]=='1' && visited[i][j]==0){
+                    dfs(i,j,grid,visited);
                     count++;
                 }
             }
         }
         return count;
     }
-
-    public void dfs(int row,int col,char[][] grid,int[][] visited,int m,int n){
+    public void dfs(int row,int col,char[][] grid,int[][] visited){
         visited[row][col]=1;
 
         int[] dx={-1,0,1,0};
         int[] dy={0,1,0,-1};
-
         for(int i=0;i<4;i++){
             int nrow=row+dx[i];
             int ncol=col+dy[i];
-
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && visited[nrow][ncol]!=1 && grid[nrow][ncol]=='1'){
-                dfs(nrow,ncol,grid,visited,m,n);
+            if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<grid[0].length && grid[nrow][ncol]=='1' && visited[nrow][ncol]==0){
+                dfs(nrow,ncol,grid,visited);
             }
         }
     }
