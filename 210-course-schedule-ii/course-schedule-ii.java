8e75c2f7636d0836fdf681f94abf74db16957ab4
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-03-06 17:43:54 -0800

    Time: 6 ms (53.21%) | Memory: 46.1 MB (49.69%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj=new ArrayList<>();
-        int [] ans=new int[numCourses];
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-
         for(int i=0;i<prerequisites.length;i++){
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
@@ -22,20 +20,20 @@ class Solution {
                 q.offer(i);
             }
         }
+        int[] ans=new int[numCourses];
         int ind=0;
         while(!q.isEmpty()){
-            int node=q.poll();
-            ans[ind]=node;
+            int ele=q.poll();
+            ans[ind]=ele;
             ind++;
-
-            for(int it : adj.get(node)){
+            for(int it : adj.get(ele)){
                 indegree[it]--;
                 if(indegree[it]==0){
                     q.offer(it);
                 }
             }
         }
-         if(ind==numCourses) return ans;
-         else return new int[]{};
+        if(ind==numCourses) return ans;
+        else return new int[]{};
     }
 }
