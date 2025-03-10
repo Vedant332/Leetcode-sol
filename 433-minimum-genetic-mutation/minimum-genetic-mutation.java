0d9b5e75208814cbb3a975364b81ea84d1f40da8
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-03-10 13:12:34 -0700

    Time: 1 ms (81.45%) | Memory: 41.5 MB (58.02%) - LeetSync

---
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -0,0 +1,43 @@
+class Pair{
+    String word;
+    int steps;
+    public Pair(String word,int steps){
+        this.word=word;
+        this.steps=steps;
+    }
+}
+class Solution {
+    char[] arr={'A','C','G','T'};
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        HashSet<String> hs=new HashSet<>();
+        for(int i=0;i<bank.length;i++){
+            hs.add(bank[i]);
+        }
+        if (!hs.contains(endGene)) return -1;
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair(startGene,0));
+        while(!q.isEmpty()){
+            String gene=q.peek().word;
+            int noOfSteps=q.peek().steps;
+            q.poll();
+
+            if(gene.equals(endGene)) return noOfSteps;
+
+            char[] charArray=gene.toCharArray();
+            for(int i=0;i<charArray.length;i++){
+                char originalChar=charArray[i];
+                for(int j=0;j<arr.length;j++){
+                    if(arr[j]==originalChar) continue;
+                    charArray[i]=arr[j];
+                    String newWord=new String(charArray);
+                    if(hs.contains(newWord)){
+                        hs.remove(newWord);
+                        q.offer(new Pair(newWord,noOfSteps+1));
+                    }
+                    charArray[i] = originalChar;   
+                }
+            }
+        }
+        return -1;
+    }
+}
