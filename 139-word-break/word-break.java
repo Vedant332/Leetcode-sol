dcb7d666aaeb9bbba3217f67899f54332d194307
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-04-09 11:48:27 -0700

    Time: 7 ms (50.91%) | Memory: 44.6 MB (37.69%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashMap<String,Boolean> map= new HashMap<>();
-        Boolean[] dp=new Boolean[s.length()+1];
-        for(String it : wordDict){
-            map.put(it,true);
+        HashMap<String,Boolean> map=new HashMap<>();
+        Boolean[] dp = new Boolean[s.length() + 1];
+    
+        for(int i=0;i<wordDict.size();i++){
+            map.put(wordDict.get(i),true);
         }
-
-        return func(s,0,map,dp);
+        return func(0,s,map,dp);
     }
 
-    public boolean func(String s,int ind,HashMap<String,Boolean> map,Boolean[] dp){
+    public boolean func(int ind,String s,HashMap<String,Boolean> map,Boolean[] dp){
         if(ind==s.length()) return true;
-        if(dp[ind]!=null) return dp[ind];
+        if(dp[ind]!=null) return dp[ind]; 
         for(int i=ind+1;i<=s.length();i++){
             if(map.containsKey(s.substring(ind,i))){
-                if(func(s,i,map,dp)){
+                if(func(i,s,map,dp)){
                     return dp[ind]=true;
                 }
             }
