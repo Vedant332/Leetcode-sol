540159299e0a66cb98e31ca556fc293d8756dad4
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-04-14 15:07:56 -0700

    Time: 6 ms (12.96%) | Memory: 47.8 MB (45.70%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,10 +3,24 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int[][] dp= new int[m][n];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=grid[i][j]+dp[i-1][j];
+                    else up=(int) 1e9;
+                    if(j>0) left=grid[i][j]+dp[i][j-1];
+                    else left=(int) 1e9;
+
+                    dp[i][j]=Math.min(up,left);
+                }
+            }
         }
-        return func(m-1,n-1,grid,dp);
+        return dp[m-1][n-1];
     }
 
     public int func(int i,int j, int[][] grid,int[][] dp){
