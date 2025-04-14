c31b3d5299e1cddf42ef8ed8213cae10fc8c96eb
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-04-14 15:27:15 -0700

    Time: 1 ms (19.64%) | Memory: 41.8 MB (65.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,11 +3,22 @@ class Solution {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         int[][] dp=new int[m][n];
-        for(int[]row : dp){
-            Arrays.fill(row,-1);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0 && obstacleGrid[i][j]!=1) dp[i][j]=1;
+                else{
+                    int up=0;
+                    int down=0;
+                    if(i>0 && obstacleGrid[i][j]!=1) up=dp[i-1][j];
+                     if(j>0 && obstacleGrid[i][j]!=1)down=dp[i][j-1];
+
+                     dp[i][j]=up+down;
+                }
+            }
         }
 
-        return func(m-1,n-1,obstacleGrid,dp);
+        return dp[m-1][n-1];
     }
 
     public int func(int i,int j,int[][] arr,int[][] dp){
