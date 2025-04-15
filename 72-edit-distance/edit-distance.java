2c9853c10d761ef85262051bc74fbb6faf647437
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-04-15 15:23:14 -0700

    Time: 2 ms (100.00%) | Memory: 44.9 MB (87.51%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,23 +2,24 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m=word1.length();
         int n=word2.length();
-        int[][] dp=new int[m+1][n+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int[][] dp=new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return func(m,n,word1,word2,dp);
+        return func(m-1,n-1,word1,word2, dp);
     }
 
-    public int func(int i,int j,String s, String t,int[][] dp){
-        if(i==0) return j;
-        if(j==0) return i;
-
+    public int func(int i,int j,String word1,String word2,int[][] dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            return func(i-1,j-1,s,t,dp);
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j]=func(i-1,j-1,word1,word2,dp);
         }
+        int one=func(i,j-1,word1,word2,dp);
+        int two=func(i-1,j,word1,word2,dp);
+        int three=func(i-1,j-1,word1,word2,dp);
 
-        return dp[i][j]=1+Math.min(func(i-1,j-1,s,t,dp),Math.min(func(i-1,j,s,t,dp),func(i,j-1,s,t,dp)));
+        return dp[i][j]=1+Math.min(one,Math.min(two,three));
     }
 }
