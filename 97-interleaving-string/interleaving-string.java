5e47f392c0689cc8ecc893913108f324bc47a24b
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2025-04-17 15:17:38 -0700

    Time: 9 ms (14.61%) | Memory: 42.1 MB (29.24%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-       if (s1.length() + s2.length() != s3.length()) return false;
-      
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-
-        dp[s1.length()][s2.length()] = true;
+        if(s1.length()+s2.length()!=s3.length()) return false;
+        boolean[][] dp=new boolean[s1.length()+1][s2.length()+1];
+        dp[s1.length()][s2.length()]=true;
 
         for (int i = s1.length(); i >= 0; i--) {
             for (int j = s2.length(); j >= 0; j--) {
@@ -17,23 +15,22 @@ class Solution {
             }
         }
 
-        // The result will be in dp[0][0]
         return dp[0][0];
     }
 
-    private boolean helper(String s1, String s2, String s3, int i, int j,boolean[][] dp) {
-        if (i == s1.length() && j == s2.length()) return true;
+    public boolean func(String s1, String s2, String s3,int i,int j,boolean[][] dp){
+        if(i==s1.length() && j==s2.length()) return true;
 
         if(dp[i][j]!=false) return dp[i][j];
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)) {
-            if (helper(s1, s2, s3, i + 1, j,dp)) {
 
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(i+j)){
+            if(func(s1,s2,s3,i+1,j,dp)){
                 return dp[i][j]=true;
             }
         }
 
-        if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j)) {
-            if (helper(s1, s2, s3, i, j + 1,dp)) {
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(i+j)){
+            if(func(s1,s2,s3,i,j+1,dp)){
                 return dp[i][j]=true;
             }
         }
