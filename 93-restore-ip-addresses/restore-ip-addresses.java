556e11cb7590b0309521eab447178452dad7b500
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-16 14:10:40 -0800

    Time: 5 ms (41.07%) | Memory: 50 MB (24.91%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -2,32 +2,30 @@ class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> ans=new ArrayList<>();
         if(s.length()>12 || s.length()<4) return ans;
-        helper(s,0,ans,"",0);
+        helper(0,s,ans,"",0);
         return ans;
     }
 
-    public void helper(String s,int ind, List<String> ans,String temp,int segment){
-        if(ind==s.length() && segment==4){
+    public void helper(int ind,String s,List<String> ans, String temp,int parts){
+        if(ind==s.length() && parts==4){
             ans.add(temp.substring(0,temp.length()-1));
             return;
         }
 
-        if(segment>=4||ind>=s.length()){
-            return ;
-        }
+        if(parts>=4|| ind>s.length()) return;
+
+
         for(int i=1;i<=3;i++){
             if(ind+i>s.length()) break;
-            String part=s.substring(ind,ind+i);
-            if(isValid(part)){
-                    helper(s,ind+i,ans,temp+part+".",segment+1);
+            if(isValid(s.substring(ind,ind+i))){
+                helper(ind+i,s,ans,temp+s.substring(ind,ind+i)+".",parts+1);
             }
         }
-        
     }
 
-    public boolean isValid(String s){
-        if(s.length()>1 && s.startsWith("0")) return false;
-        int num=Integer.parseInt(s);
+    public boolean isValid(String t){
+        if(t.length()>1 && t.startsWith("0")) return false;
+        int num=Integer.parseInt(t);
         return num>=0 && num<=255;
     }
 }
