c904cdd836637a80c04e4be4aaa80082dbe73c19
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-19 16:25:32 -0800

    Time: 3 ms (22.77%) | Memory: 42.9 MB (61.93%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -5,27 +5,27 @@ class Solution {
         String ans="";
 
         for(int i=0;i<s.length();i++){
-           if(Character.isDigit(s.charAt(i))){
-            int count=0;
-            while(Character.isDigit(s.charAt(i))){
-                count=10*count+(s.charAt(i)-'0');
+            if(Character.isDigit(s.charAt(i))){
+               int count=0;
+               while(Character.isDigit(s.charAt(i))){
+                count=10*count + (s.charAt(i)-'0');
                 i++;
+               }
+               num.push(count);
+               i--;
+            }else if(s.charAt(i)=='['){
+                res.push(ans);
+                ans="";
+            }else if(s.charAt(i)==']'){
+                StringBuilder sb=new StringBuilder(res.pop());
+                int count=num.pop();
+                for(int j=0;j<count;j++){
+                    sb.append(ans);
+                }
+                ans=sb.toString();
+            }else{
+                ans+=s.charAt(i);;
             }
-            i--;
-            num.push(count);
-           }else if(s.charAt(i)=='['){
-            res.push(ans);
-            ans="";
-           }else if(s.charAt(i)==']'){
-            StringBuilder sb=new StringBuilder(res.pop());
-            int count=num.pop();
-            for(int j=0;j<count;j++){
-                sb.append(ans);
-            }
-            ans=sb.toString();
-           }else{
-            ans+=s.charAt(i);
-           }
         }
         return ans;
     }
