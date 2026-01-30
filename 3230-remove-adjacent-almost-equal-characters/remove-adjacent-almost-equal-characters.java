1e9d269826987ea8ab03bb5ebad3834f830af08d
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-30 11:36:59 -0800

    Time: 1 ms (99.12%) | Memory: 43.7 MB (42.98%) - LeetSync

--- 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.java
+++ 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.java
@@ -1,16 +1,12 @@
 class Solution {
     public int removeAlmostEqualCharacters(String word) {
-        int i=1;
         int count=0;
-        int n= word.length();
-
-        while(i<n){
-            if(Math.abs(word.charAt(i)-word.charAt(i-1))<=1 ){
+        
+        for(int i=1;i<word.length();i++){
+            if(Math.abs(word.charAt(i)-word.charAt(i-1))<=1){
                 count++;
-                i=i+2;
-                continue;
+                i=i+1;
             }
-            i++;
         }
         return count;
     }
