3fe24bdad87ee18948c6212fc8a3c474c4035f26
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-31 15:23:55 -0800

    Time: 2 ms (90.81%) | Memory: 44.6 MB (14.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -8,27 +8,27 @@ class Pair{
         this.time=time;
     }
 }
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        
-        int[][] visited=new int[m][n];
-        Queue<Pair> q=new LinkedList<>();
 
-        int cntFrsh=0;
+        Queue<Pair> q=new LinkedList<>();
+        int[][] vis=new int[m][n];
+        int cntFresh=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
+                    vis[i][j]=1;
                     q.offer(new Pair(i,j,0));
-                    visited[i][j]=1;
                 }else if(grid[i][j]==1){
-                    cntFrsh++;
+                    cntFresh++;
                 }
             }
         }
 
-        int ct=0;
+        int cnt=0;
         int minTm=Integer.MIN_VALUE;
 
         while(!q.isEmpty()){
@@ -41,19 +41,18 @@ class Solution {
 
             int[] dx={-1,0,1,0};
             int[] dy={0,1,0,-1};
-
             for(int i=0;i<4;i++){
                 int nrow=r+dx[i];
                 int ncol=c+dy[i];
-
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && visited[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && vis[nrow][ncol]!=1){
                     q.offer(new Pair(nrow,ncol,t+1));
-                    visited[nrow][ncol]=1;
-                    ct++;
+                    cnt++;
+                    vis[nrow][ncol]=1;
                 }
             }
         }
-        if(cntFrsh!=ct) return -1;
+
+        if(cnt!=cntFresh) return -1;
         else return (minTm==Integer.MIN_VALUE)?0:minTm ;
     }
 }
