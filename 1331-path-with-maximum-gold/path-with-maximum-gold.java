f8964e6af8b31c5734b98e340b791159788412d1
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-31 19:25:24 -0800

    Time: 97 ms (16.78%) | Memory: 47.2 MB (6.01%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -3,32 +3,32 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int maxi=0;
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]!=0){
-                    maxi=Math.max(maxi,dfs(i,j,grid,m,n));
+                    maxi=Math.max(maxi,dfs(i,j,m,n,grid,new int[m][n]));
                 }
             }
         }
         return maxi;
     }
 
-    public int dfs(int row,int col,int[][] grid,int m,int n){
-        int gold=grid[row][col];
+    public int dfs(int row,int col,int m,int n,int[][] grid,int[][] vis){
+        vis[row][col]=1;
 
-        grid[row][col]=101;
-        int maxGold=0;
+        int gold=0;
 
-        int[] dx={-1,0,1,0};
+         int[] dx={-1,0,1,0};
         int[] dy={0,1,0,-1};
         for(int i=0;i<4;i++){
             int nrow=row+dx[i];
             int ncol=col+dy[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]!=101 && grid[nrow][ncol] != 0){
-                maxGold=Math.max(maxGold,dfs(nrow,ncol,grid,m,n));
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]!=0 && vis[nrow][ncol]==0){
+                gold=Math.max(gold,dfs(nrow,ncol,m,n,grid,vis));
             }
         }
-        grid[row][col]=gold;
-        return maxGold+gold;
+        vis[row][col] = 0;
+        return grid[row][col]+gold;
     }
 }
