312612b408bd17a03a85d1020e0e3ada2a9bf045
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-31 18:49:20 -0800

    Time: 8 ms (94.91%) | Memory: 47.8 MB (7.07%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.java
+++ 971-shortest-bridge/shortest-bridge.java
@@ -1,72 +1,70 @@
-class Pair {
+class Pair{
     int row;
     int col;
-
-    public Pair(int row, int col) {
-        this.row = row;
-        this.col = col;
+    public Pair(int row,int col){
+        this.row=row;
+        this.col=col;
     }
 }
-
 class Solution {
 
-    public void dfs(int r, int c, boolean[][] vis, int[][] grid, int m, int n, Queue<Pair> q) {
-        vis[r][c] = true;
-        q.offer(new Pair(r, c));  
+    public void dfs(int r,int c,int m,int n,int[][] grid,int[][] vis,Queue<Pair> q){
+        vis[r][c]=1;
+        q.offer(new Pair(r,c));
+
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
-        for (int i = 0; i < 4; i++) {
-            int nrow = r + dx[i];
-            int ncol = c + dy[i];
-            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
-                dfs(nrow, ncol, vis, grid, m, n, q);
+        for(int i=0;i<4;i++){
+            int nrow=r+dx[i];
+            int ncol=c+dy[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && vis[nrow][ncol]!=1){
+                dfs(nrow,ncol,m,n,grid,vis,q);
             }
         }
     }
-
     public int shortestBridge(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        Queue<Pair> q = new LinkedList<>();
-        boolean flag = false;
+        int m=grid.length;
+        int n=grid[0].length;
+        int[][] vis=new int[m][n];
+        Queue<Pair> q=new LinkedList<>();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    dfs(i, j, visited, grid, m, n, q);
-                    flag = true;
+        boolean flag=false;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    dfs(i,j,m,n,grid,vis,q);
+                    flag=true;
                     break;
                 }
             }
-            if (flag) break;
+            if(flag==true) break;
         }
-
-        int steps = 0;
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
+        int steps=0;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int l = 0; l < size; l++) {
-                Pair current = q.poll();
-                int currentRow = current.row;
-                int currentCol = current.col;
+        while(!q.isEmpty()){
 
-                for (int k = 0; k < 4; k++) {
-                    int nrow = currentRow + dx[k];
-                    int ncol = currentCol + dy[k];
-                    if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !visited[nrow][ncol]) {
-                        if (grid[nrow][ncol] == 1) {
-                            return steps;  
-                        }
-                        visited[nrow][ncol] = true;
-                        q.offer(new Pair(nrow, ncol));
+            int size=q.size();
+            for(int j=0;j<size;j++){
+                int r=q.peek().row;
+                int c=q.peek().col;
+                q.poll();
+
+            for(int i=0;i<4;i++){
+                int nrow=r+dx[i];
+                int ncol=c+dy[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0){
+                    if(grid[nrow][ncol]==1){
+                        return steps;
                     }
+                    q.offer(new Pair(nrow,ncol));
+                    vis[nrow][ncol]=1;
                 }
+            }
             }
             steps++;
         }
         return -1;
     }
-}
+} 
