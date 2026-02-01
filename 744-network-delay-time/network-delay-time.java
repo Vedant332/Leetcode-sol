8d04a3f6ec4e9b39ca8f58b68254eafb7099dd4a
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-01-31 22:54:28 -0800

    Time: 10 ms (84.13%) | Memory: 49.1 MB (60.93%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,49 +1,45 @@
 class Pair{
-    int node;
-    int wt;
-    public Pair(int node,int wt){
-        this.node=node;
-        this.wt=wt;
+    int dest;
+    int weight;
+    public Pair(int dest,int weight){
+        this.dest=dest;
+        this.weight=weight;
     }
 }
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int m=times.length;
         List<List<Pair>> adj=new ArrayList<>();
+
         for(int i=0;i<n+1;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int i=0;i<m;i++){
-            int u=times[i][0];
-            int v=times[i][1];
-            int w=times[i][2];
-            adj.get(u).add(new Pair(v,w));
+        for(int i=0;i<times.length;i++){
+            adj.get(times[i][0]).add(new Pair(times[i][1],times[i][2]));
         }
-        PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)->x.wt-y.wt);
 
+        PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)->x.weight-y.weight);
         int[] dist=new int[n+1];
         Arrays.fill(dist,(int)1e9);
         dist[k]=0;
         pq.add(new Pair(k,0));
+
         while(!pq.isEmpty()){
-            int ele=pq.peek().node;
-            int tm=pq.peek().wt;
+            int node=pq.peek().dest;
+            int wt=pq.peek().weight;
             pq.remove();
 
-            for(Pair it : adj.get(ele)){
-                int adjNode=it.node;
-                int edgeWt=it.wt;
-                if(dist[adjNode]>edgeWt+tm){
-                    dist[adjNode]=edgeWt+tm;
-                    pq.add(new Pair(adjNode,edgeWt+tm));
+            for(Pair it : adj.get(node)){
+                if(it.weight+wt<dist[it.dest]){
+                    dist[it.dest]=it.weight+wt;
+                    pq.add(new Pair(it.dest,it.weight+wt));
                 }
             }
         }
-        int ans=dist[1];
-        for(int i=1;i<=n;i++){
+        int ans=0;
+        for(int i=1;i<dist.length;i++){
             if(dist[i]==(int)1e9) return -1;
-            else ans=Math.max(ans,dist[i]);
+            else ans =Math.max(dist[i],ans);
         }
         return ans;
     }
