503f2e401877adff0b882952d50beefc00700b47
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-02-23 18:26:30 -0800

    Time: 0 ms (100.00%) | Memory: 44.9 MB (27.15%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,9 +7,10 @@ class Solution {
             int mid=lo+(hi-lo)/2;
 
             if(nums[mid]==target) return mid;
-            else if(nums[mid]<target) lo=mid+1;
             else if(nums[mid]>target) hi=mid-1;
+            else lo=mid+1;
         }
+
         return lo;
     }
 }
