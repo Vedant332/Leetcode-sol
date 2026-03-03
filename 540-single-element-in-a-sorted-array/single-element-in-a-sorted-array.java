c255235edbee8b2f4d1105b9c9e5d7835ce1950e
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-03-03 15:13:25 -0800

    Time: 0 ms (100.00%) | Memory: 52.7 MB (90.35%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -11,7 +11,7 @@ class Solution {
             int mid=lo+(hi-lo)/2;
 
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])) lo=mid+1;
+            else if(mid%2==1 && nums[mid]==nums[mid-1] || mid%2==0 && nums[mid]==nums[mid+1]) lo=mid+1;
             else hi=mid-1;
         }
         return -1;
