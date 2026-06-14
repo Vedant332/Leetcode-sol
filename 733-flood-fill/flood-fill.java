c070267fdebdbe3ebe4fac1549902592b9d4b72c
vedant raj kavi <52864168+Vedant332@users.noreply.github.com>
2026-06-14 15:15:50 -0700

    Time: 1 ms (38.05%) | Memory: 46.7 MB (60.52%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,28 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {    
         int m=image.length;
         int n=image[0].length;
-        int[][] visited=new int[m][n];
         int prevColor=image[sr][sc];
-        if(prevColor==color) return image;
-        dfs(image,sr,sc,color,m,n,prevColor);
 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==sr && j==sc){
+                    dfs(image,sr,sc,color,m,n,prevColor);
+                }
+            }
+        }
         return image;
     }
 
-    public void dfs(int[][] image,int row,int col,int color,int m, int n, int prevColor){
-        image[row][col]=color;
+    public void dfs(int[][] image, int sr,int sc,int color,int m,int n,int prevColor){
+        image[sr][sc]=color;
 
-        int[] dx={-1,0,1,0};
-        int[] dy={0,1,0,-1};
-
+        int[] dx={0,-1,0,1};
+        int[] dy={-1,0,1,0};
         for(int i=0;i<4;i++){
-            int nrow=row+dx[i];
-            int ncol=col+dy[i];
-
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && image[nrow][ncol]==prevColor){
+            int nrow=dx[i]+sr;
+            int ncol=dy[i]+sc;
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && image[nrow][ncol]!=color && image[nrow][ncol]==prevColor){
                 dfs(image,nrow,ncol,color,m,n,prevColor);
             }
         }
